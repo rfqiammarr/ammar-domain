836796a0f25e3e44e7206454242ebd5a1e232ab1
Mas Ammar <rifqiammarramadhan103@gmail.com>
2025-04-08 11:51:07 +0700

    Fix

---
+++ app/Home/imgCard.js
@@ -0,0 +1,13 @@
+const imgCard = () => {
+  return (
+    <>
+      <div class="max-w-2xs bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <img class="rounded-t-lg" src="/img/profile/rams8bit.png" alt="Ammar 8bits" />
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default imgCard;
--- app/page.js
+++ app/page.js
@@ -1,3 +1,17 @@
+import ImgCard from "./Home/imgCard";
+import BioCard from "./Home/BioCard";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className="flex flex-col items-center text-center">
+        <h2 className="text-3xl font-semibold text-gray-900 dark:text-white mb-3">Hello!👋 I&apos;m Rifqi Ammar Ramadhan a Software developer</h2>
+        <h2 className="text-3xl font-semibold text-gray-900 dark:text-white mb-10">from Indonesia</h2>
+        <ImgCard />
+        <div className="mt-6">
+          <BioCard />
+        </div>
+      </div>
+    </>
+  );
 }
