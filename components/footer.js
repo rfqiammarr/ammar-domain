aa8b29d49b5423561af7f3477035f3f1e44b02d3
Mas Ammar <rifqiammarramadhan103@gmail.com>
2025-04-09 19:54:13 +0700

    Fix

--- components/footer.js
+++ components/footer.js
@@ -14,9 +14,9 @@ const footer = () => {
           </span>
           <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
-              <a href="/" className="hover:underline me-4 md:me-6">
-                Home
-              </a>
+              <Link href="/about">
+                <a className="hover:underline me-4 md:me-6">Home</a>
+              </Link>
             </li>
             <li>
               <Link href="/about">
