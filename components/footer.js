cf14ccc40d01eb5d17bfe1f2e39ec8252f2a7e85
Mas Ammar <rifqiammarramadhan103@gmail.com>
2025-04-09 17:47:25 +0700

    Fix

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 const footer = () => {
   return (
     <>
-      <footer className="bg-white rounded-lg shadow-sm m-4 dark:bg-gray-800 text-center mt-8">
+      <footer className="m-4 dark:bg-gray-800 text-center mt-8">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2025{" "}
@@ -12,22 +12,22 @@ const footer = () => {
           </span>
           <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
+              <a href="/" className="hover:underline me-4 md:me-6">
                 Home
               </a>
             </li>
             <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
+              <a href="/about" className="hover:underline me-4 md:me-6">
                 About
               </a>
             </li>
             <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
+              <a href="/blog" className="hover:underline me-4 md:me-6">
                 Blog
               </a>
             </li>
             <li>
-              <a href="#" className="hover:underline">
+              <a href="/project" className="hover:underline">
                 Project
               </a>
             </li>
