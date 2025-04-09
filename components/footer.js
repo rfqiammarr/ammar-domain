77cf531e58e917bd8de06072987ba59d1eba34a1
Mas Ammar <rifqiammarramadhan103@gmail.com>
2025-04-09 20:01:56 +0700

    Fix

--- components/footer.js
+++ components/footer.js
@@ -7,30 +7,33 @@ const footer = () => {
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2025{" "}
-            <a href="https://rifqiammarramadhan.com/" className="hover:underline">
+            <Link href="/">
+              <p href="/" className="hover:underline">
+                {" "}
+              </p>
               Rifqi Ammar Ramadhan
-            </a>
+            </Link>
             . All Rights Reserved.
           </span>
           <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
-              <Link href="/about">
-                <a className="hover:underline me-4 md:me-6">Home</a>
+              <Link href="/">
+                <p className="hover:underline me-4 md:me-6">Home</p>
               </Link>
             </li>
             <li>
               <Link href="/about">
-                <a className="hover:underline me-4 md:me-6">About</a>
+                <p className="hover:underline me-4 md:me-6">About</p>
               </Link>
             </li>
             <li>
               <Link href="/blog">
-                <a className="hover:underline me-4 md:me-6">Blog</a>
+                <p className="hover:underline me-4 md:me-6">Blog</p>
               </Link>
             </li>
             <li>
               <Link href="/project">
-                <a className="hover:underline">Project</a>
+                <p className="hover:underline">Project</p>
               </Link>
             </li>
           </ul>
